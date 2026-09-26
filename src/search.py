62d5566c5ea8884990a45c81fa028c9c758907e4
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2026-09-26 21:23:46 +0200

    LLM ollama and Bielik added, first questions nad context sent

--- src/search.py
+++ src/search.py
@@ -3,6 +3,7 @@ import json
 import faiss
 from sentence_transformers import SentenceTransformer, CrossEncoder
 import torch
+import ollama
 
 CHUNKS_PATH = Path("data/processed/rav4_chunks.json")
 INDEX_PATH = Path("data/processed/rav4_faiss.index")
@@ -19,7 +20,13 @@ RERANKER_NAME = "BAAI/bge-reranker-v2-m3"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL = SentenceTransformer(MODEL_NAME, device=device)
 
-QUERY = "Jak wyłączyć system PCS?"
+# QUERY = "Jak wyłączyć system PCS?"
+# QUERY = "Jak odblokować lewarek zmiany biegów w razie awarii?"
+# QUERY = "Jak oblokować ręcznie klapę bagażnika w przypadku rozładowania akumulatora?" 
+# QUERY = "Jak wyregulować fotel kierowcy?"
+# QUERY = "Jak wymienić żarówkę kierunkowskazu prawy przód?"
+# QUERY = "Jaka żarówka do kierunkowskazu przód?"
+QUERY = "Jak ugotować bigos?"
 
 query = "query: " + QUERY
 
@@ -33,14 +40,13 @@ k = 20
 
 scores, indices = index.search(query_embedding, k)
 
-
 pairs = []
 
-print(scores)
-print(indices)
+# print(scores)
+# print(indices)
 
-for indice, score in zip(indices[0], scores[0]):
-    print(indice, score)
+# for indice, score in zip(indices[0], scores[0]):
+#     print(indice, score)
 
 for indice in indices[0]:
     text = chunks[indice]["text"]
@@ -66,8 +72,30 @@ for indice, reranker_score in top_results:
 
 context = "\n\n".join(context_parts)
 
-print(context)
+LLM_MODEL = "SpeakLeash/bielik-11b-v2.3-instruct:Q8_0"
 
+SYSTEM_PROMPT = """Jesteś asystentem technicznym.
+Odpowiadaj na pytanie wyłącznie na podstawie dostarczonego kontekstu.
+Jeżeli w kontekście nie ma odpowiedzi, powiedz, że nie znalazłeś
+tej informacji w dostarczonej dokumentacji."""
+
+USER_PROMPT = f"""Kontekst:
+{context}
+
+Pytanie: 
+{QUERY}"""
+
+messages = [
+    {"role": "system", "content": SYSTEM_PROMPT},
+    {"role": "user", "content": USER_PROMPT}
+] 
+
+response = ollama.chat(
+    model=LLM_MODEL,
+    messages=messages
+)
+
+print(response["message"]["content"])
 
 # for chunk in chunks:
 #     if "PCS" in chunk["text"] and "wyłącz" in chunk["text"]:
