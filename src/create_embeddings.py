675b6a5e3fddbe4bdb50c510aa55473cedbef3e7
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2026-09-26 14:25:20 +0200

    Index created index exported to rav4_faiss.index

--- src/create_embeddings.py
+++ src/create_embeddings.py
@@ -2,10 +2,12 @@ from pathlib import Path
 import json
 from sentence_transformers import SentenceTransformer
 import torch
-
-# print(torch.cuda.is_available())
+import numpy as np
+import faiss
 
 INPUT_PATH = Path("data/processed/rav4_chunks.json")
+
+INDEX_PATH = Path("data/processed/rav4_faiss.index")
 
 MODEL_NAME = "intfloat/multilingual-e5-base"
 
@@ -14,8 +16,6 @@ with open(INPUT_PATH, "r", encoding="utf-8")as file:
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL = SentenceTransformer(MODEL_NAME, device=device)
-
-# print(MODEL.device)
 
 texts = []
 
@@ -28,4 +28,13 @@ embeddings = MODEL.encode(
     show_progress_bar=True
     )
 
-print(embeddings.shape)
+dimension = embeddings.shape[1]
+
+index = faiss.IndexFlatIP(dimension)
+
+index.add(embeddings)
+
+faiss.write_index(index, str(INDEX_PATH))
+
+# similarity = np.dot(embeddings[0], embeddings[1])
+# print(similarity)
